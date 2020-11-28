0db2c9955618e3b87c1617c5e4db9acd5b409627
Adam Schneit <aschneit@gmail.com>
2020-11-27 22:07:14 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-A tool for small business owners to calculate their employee healthcare savings under the New York Health Act.
---
+++ precache-manifest.63225f63a1b87f1ae4cca68b30cfbf19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf960481e1bab0d5e6175bd338f8248",
+    "url": "/NYHABusinessCalculator/index.html"
+  },
+  {
+    "revision": "3e8885ad7d383bd85b4b",
+    "url": "/NYHABusinessCalculator/static/css/main.399f3665.chunk.css"
+  },
+  {
+    "revision": "b3919700dda5d5c8a8b5",
+    "url": "/NYHABusinessCalculator/static/js/2.219f707f.chunk.js"
+  },
+  {
+    "revision": "ac59a904fc44e204ca9e3114da0ce949",
+    "url": "/NYHABusinessCalculator/static/js/2.219f707f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e8885ad7d383bd85b4b",
+    "url": "/NYHABusinessCalculator/static/js/main.c8d2a4d8.chunk.js"
+  },
+  {
+    "revision": "3645cc5d3f10b2ad8be9",
+    "url": "/NYHABusinessCalculator/static/js/runtime-main.01bac343.js"
+  },
+  {
+    "revision": "07aa0cad7f7425fb8a0cdd5e0909c616",
+    "url": "/NYHABusinessCalculator/static/media/campaign.07aa0cad.png"
+  }
+]);
